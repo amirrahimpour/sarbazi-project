22e3b9ad0aaa948f1e5e0f75fcda57a8be160077
amirrahimpour <112168420+amirrahimpour@users.noreply.github.com>
2022-08-31 22:51:10 +0430

    Update run.py
    add comments

--- run.py
+++ run.py
@@ -6,17 +6,18 @@ from graph_handler import GraphHandler
 from logs.log_manager import LogManager
 
 def main(log_file_name):
-    logger = LogManager()
+    """ main function of the application """
+    logger = LogManager()                                           # initialize logger object                    
     credentials = {"uri": "bolt://localhost:7687",
                    "userName": "neo4j", "password": "test"}
 
-    log_filter = LogFilter()
-    neo = Neo4jHandler(credentials)
+    log_filter = LogFilter()                                        # initialize log filter object
+    neo = Neo4jHandler(credentials)                                 # initialize neo4j handler object
 
-    file_extension = log_file_name.split(".")[1]
+    file_extension = log_file_name.split(".")[1]                    # extract input file extension
     if file_extension == "txt":
         try:
-            with open(log_file_name) as f:
+            with open(log_file_name) as f:                          # split into lines
                 lines = f.read().split("\n")
         except FileNotFoundError as e:
             print(f"{e}")
@@ -29,20 +30,20 @@ def main(log_file_name):
             print(f"{e}")
             exit(2)
 
-    graph_handler = GraphHandler(neo, log_filter, logger)
+    graph_handler = GraphHandler(neo, log_filter, logger)           # initialize graph handler object
     if file_extension == "txt":
-        graph_handler.create_graph_txt(lines)
+        graph_handler.create_graph_txt(lines)                       # call function for creating graph from .txt file
 
     if file_extension == "json":
-        graph_handler.create_graph_json(lines)
+        graph_handler.create_graph_json(lines)                      # call function for creating graph from .json file
 
     print("finished")
 
 
 if __name__ == "__main__":
     log_file_name = None
-    if len(sys.argv) > 1:
-        if sys.argv[1] == "--help":
+    if len(sys.argv) > 1:                                           # check if input file name is given    
+        if sys.argv[1] == "--help":                                 # print the manual if --help is given
             print(
                 """
                 transform logs into Neo4j graphs
@@ -50,7 +51,7 @@ if __name__ == "__main__":
                 """
             )
         else:
-            log_file_name = sys.argv[1]
+            log_file_name = sys.argv[1]                             # extract file name from input
             main(log_file_name)
     else:
         print(
